c08483ac3cb4250bd79468273776339fc8fc8793
lenildodamasceno <lenildo58479636@edum.rn.senac.br>
2025-08-18 22:52:51 -0300

    Adiciona arquivos resposta1 e resposta2

---
+++ resposta1.js
@@ -0,0 +1,8 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+let idade,dias
+
+idade = prompt("Digite sua idade: ");
+dias = idade * 365;
+console.log("Você viveu aproximadamente " + dias + " dias.");
+
