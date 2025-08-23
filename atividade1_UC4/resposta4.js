c6c1ee98f58fabcfd6bf86c90e77068966e65969
lenildodamasceno <lenildo58479636@edum.rn.senac.br>
2025-08-22 21:01:50 -0300

    :

--- atividade1_UC4/resposta4.js
+++ atividade1_UC4/resposta4.js
@@ -1,16 +1,34 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 let juros_pago = 0;
-console.log("Bem-vindo ao sistema de empréstimo!");
+console.log("BANCO SENAC , BORA FAZER SEU EMPRESTIMO!");
 let salario = Number(prompt("Qual o valor de seu salário? "));
 let solicitado_ep = Number(
-  prompt("Informe o valor desejado para o empréstimo => ")
+  prompt("Informe o valor desejado para o empréstimo = ")
 );
-let parcelas = Number(prompt("Informe o número de parcelas => "));
+let parcelas = Number(prompt("Informe o número de parcelas = "));
+
+let valor_parcela;
+if (parcelas > 0) {
+  valor_parcela = solicitado_ep / parcelas;
+} else {
+  valor_parcela = 0;
+}
+let emprestimo_aprovado = true;
+
+if (valor_parcela > 0.3 * salario) {
+  emprestimo_aprovado = false;
+  console.log("Empréstimo negado. O valor da parcela excede 30% do salário.");
+}
+
+if (solicitado_ep <= 0) {
+  emprestimo_aprovado = false;
+  console.log("Valor inválido para o empréstimo.");
+}
 if (solicitado_ep <= 0) {
   console.log("Valor inválido para o empréstimo.");
 }
-if (solicitado_ep / parcelas > 0.3 * salario) {
+if (parcelas > 0.3 * salario) {
   console.log("emprestimo negado.");
 } else {
   console.log("Empréstimo aprovado.");
