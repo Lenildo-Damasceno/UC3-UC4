d26df9feee0b980b0d86dcecad9226271a247c40
Lenildo-Damasceno <lenildo58479636@edum.n.senac.br>
2025-09-10 01:05:49 -0300

    ✨ Atualização:

--- funções/resposta1.js
+++ funções/resposta1.js
@@ -1,18 +1,30 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-function caminhada (tipo,tempo){
-    const caloriasPorMinuto = 5 
-    return caloriasPorMinuto * tempo;
+function CalcularCalorias (tipodeexer,tempoemMin) {
+    let exercicio = tipodeexer.toLowerCase();
+    switch (exercicio){
+        case 'caminhada':
+        return tempoemMin*5
+        case 'corrida' :
+        return tempoemMin*10;
+        case 'bicicleta':
+        return tempoemMin*8;
+        default: 
+        return console.log("Exercício não encontrado");
+ }
 }
-function corrida(tipo,tempo){
-    const caloriasPorMinuto = 10 
-    return caloriasPorMinuto * tempo;
-}
-function bicicleta(tipo,tempo){
-    const caloriasPorMinuto = 8
-    return caloriasPorMinuto * tempo;
-}
+
+
+let exercicioFeito = prompt("QUAL EXERCICIO VOCÊ REALIZOU : ");
+
+let tempoemMin = Number(prompt(`Quanto tempo você fez de ${exercicioFeito} : `));
+
+let calorias = CalcularCalorias(exercicioFeito, tempoemMin); 
+
+console.log(`Você gastou ${calorias} calorias fazendo ${exercicioFeito} por ${tempoemMin} minutos`);
+
+
 
 
     
