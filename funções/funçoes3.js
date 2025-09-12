cc536ce11cf12def3b103bcdb71024aa993f8769
lenildodamasceno <lenildo58479636@edum.rn.senac.br>
2025-09-11 21:23:15 -0300

    ✨ Atualização

--- funções/funçoes3.js
+++ funções/funçoes3.js
@@ -1,10 +1,22 @@
 import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
-export function adicionarProduto(produto) {
-    estoque.push(produto);
+
+export function listarEstoque(prod, qtde, valor) {
+    console.log (' PRODUTOS - PREÇOS - QUANTIDADE')
+    for (let i = 0; i < prod.length; i++) {
+        console.log(`Produto: ${prod[i]}, Quantidade: ${qtde[i]}, Valor: ${valor[i]}`);
+    }
 }
-export function listarEstoque() {
-    return estoque;
+
+// const produto = function(prod,nome){
+//     prod.push(nome)
+//     return
+// }
+export function adicionarEstoque(produto, nome, quantidade, qtde, preco, valor) {
+    produto.push(nome);
+    qtde.push(quantidade);
+    valor.push(preco);
 }
- 
+
+export const consultarEstoque = (produto, nome) => produto.indexOf(nome);
